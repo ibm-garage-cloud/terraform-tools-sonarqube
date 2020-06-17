8b4bc015743ffaa75d61f76ea1ec44ab61afae81
Sean Sundberg <seansund@us.ibm.com>
2020-06-16 23:46:38 -0500

    Remove quotes from urls

--- scripts/wait-for-deployment.sh
+++ scripts/wait-for-deployment.sh
@@ -7,7 +7,7 @@ DEPLOYMENT="$2"
 
 kubectl rollout status deployment "${DEPLOYMENT}" -n "${NAMESPACE}" --timeout=30m
 
-CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" -l grouping=garage-cloud-native-toolkit -l app.kubernetes.io/component=tools -o json | jq '.items[].data | to_entries | select(.[].key | endswith("_URL")) | .[].value')
+CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" -l grouping=garage-cloud-native-toolkit -l app.kubernetes.io/component=tools -o json | jq '.items[].data | to_entries | select(.[].key | endswith("_URL")) | .[].value' | sed "s/\"//g")
 
 echo "${CONFIG_URLS}" | while read url; do
   if [[ -n "${url}" ]]; then
