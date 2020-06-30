60c0367381cb952fc4602b05778f1d37613e69cf
Sean Sundberg <seansund@us.ibm.com>
2020-06-30 17:44:27 -0500

    Simplifies wait-for-deployment logic (#8)

--- scripts/wait-for-deployment.sh
+++ scripts/wait-for-deployment.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 NAMESPACE="$1"
 DEPLOYMENT="$2"
 
+set -e
+
 kubectl rollout status deployment "${DEPLOYMENT}" -n "${NAMESPACE}" --timeout=30m
 
-CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" -l grouping=garage-cloud-native-toolkit -l app.kubernetes.io/component=tools -o json | jq '.items[].data | to_entries | select(.[].key | endswith("_URL")) | .[].value' | sed "s/\"//g")
+CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" sonarqube-config -o jsonpath='{.data.SONARQUBE_URL}')
 
 echo "${CONFIG_URLS}" | while read url; do
   if [[ -n "${url}" ]]; then
