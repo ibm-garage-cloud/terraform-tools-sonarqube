92bfe8699dc82c4c5cc4b5bb3bf63b00721efcfc
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 13:46:01 -0500

    Adds ocp-vpc dependency and updates tests
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-sonarqube.tf
+++ test/stages/stage2-sonarqube.tf
@@ -1,12 +1,12 @@
 module "dev_tools_sonarqube" {
   source = "./module"
 
-  cluster_type             = module.dev_cluster.type_code
-  cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
+  cluster_type             = module.dev_cluster.platform.type_code
+  cluster_ingress_hostname = module.dev_cluster.platform.ingress
+  tls_secret_name          = module.dev_cluster.platform.tls_secret
   releases_namespace       = module.dev_capture_state.namespace
   service_account_name     = "sonarqube"
-  tls_secret_name          = module.dev_cluster.tls_secret_name
   postgresql               = {
     external      = false
     username      = ""
